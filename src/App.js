bcf04fe2a46e3a120d3fe0dd89b0d80917b3f667
hyein <miknieyh@yanolja.com>
2023-08-30 17:56:35 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -21,12 +21,22 @@ class App extends React.Component{
         this.setState({items:thisItems}); //업데이트는 반드시 this.setState로 해야 됨
         console.log("items : ",this.state.items);
     }
+
+    delete = (item) => {
+        const thisItems = this.state.items;
+        console.log("Before Update Items :",this.state.items);
+        const newItems = thisItems.filter(e=>e.id !== item.id);
+        this.setState({items : newItems},()=>{
+            //디버깅 콜백
+            console.log("Update Items :",this.state.items);
+        })
+    }
   render() {
         var todoItems = this.state.items.length > 0 && (
             <Paper style={{margin:16}}>
                 <List>
                     {this.state.items.map((item, idx)=> (
-                        <Todo item = {item} key ={item.id}/>
+                        <Todo item = {item} key ={item.id} delete = {this.delete}/>
                     ))}
                 </List>
             </Paper>
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from '@material-ui/core';
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from '@material-ui/core';
+import {DeleteOutline} from "@material-ui/icons";
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = { item: props.item };
+        this.delete = props.delete;
+    }
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
     }
     render() {
         const item = this.state.item;
@@ -22,6 +27,12 @@ class Todo extends React.Component {
                         fullWidth={true}
                     />
                 </ListItemText>
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete Todo"
+                    onClick={this.deleteEventHandler}>
+                        <DeleteOutline/>
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
