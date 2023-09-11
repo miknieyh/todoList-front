ec716e78b8e3f7711a3f4fa2499ceef2acfee0f8
miknieyh <tong_hye@naver.com>
2023-09-11 15:44:55 +0900

    fetch API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Todo from './Todo';
 import AddTodo from "./AddTodo";
 import {Paper, List, Container} from '@material-ui/core';
 import './App.css';
+import {call} from "./service/ApiService";
 
 class App extends React.Component{
     constructor(props) {
@@ -14,43 +15,39 @@ class App extends React.Component{
     }
     //함수 추가
     add = (item) => {
-        const thisItems = this.state.items;
-        item.id = "ID-" + this.state.items.length; //key를 위한 id 추가
-        item.done = false;
-        thisItems.push(item); //리스트에 아이템 추가
-        this.setState({items:thisItems}); //업데이트는 반드시 this.setState로 해야 됨
-        console.log("items : ",this.state.items);
+        call("/todo","POST",item).then((response)=>
+            this.setState({items:response.data})
+        )
+        // const thisItems = this.state.items;
+        // item.id = "ID-" + this.state.items.length; //key를 위한 id 추가
+        // item.done = false;
+        // thisItems.push(item); //리스트에 아이템 추가
+        // this.setState({items:thisItems}); //업데이트는 반드시 this.setState로 해야 됨
+        // console.log("items : ",this.state.items);
     }
 
     delete = (item) => {
-        const thisItems = this.state.items;
-        console.log("Before Update Items :",this.state.items);
-        const newItems = thisItems.filter(e=>e.id !== item.id);
-        this.setState({items : newItems},()=>{
-            //디버깅 콜백
-            console.log("Update Items :",this.state.items);
-        })
+        call("/todo","DELETE",item).then((response) =>
+            this.setState({items:response.data})
+        );
+        // const thisItems = this.state.items;
+        // console.log("Before Update Items :",this.state.items);
+        // const newItems = thisItems.filter(e=>e.id !== item.id);
+        // this.setState({items : newItems},()=>{
+        //     //디버깅 콜백
+        //     console.log("Update Items :",this.state.items);
+        // })
     }
-    componentDidMount() {
-        const requestOptions={
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json"
-            }
-        };
 
-        fetch("http://localhost:8080/todo",requestOptions)
-            .then((response)=>
-                response.json()
-            ).then(
-                (response) => {
-                this.setState({
-                    items: response.data,
-                });
-            },
-                (error)=>{
-                    this.setState({error,});
-                });
+    update = (item) => {
+        call("/todo","PUT",item).then((response) =>
+            this.setState({items:response.data})
+        );
+    };
+    componentDidMount() {
+       call("/todo","GET",null).then((response) =>
+       this.setState({items : response.data })
+       );
     }
 
     render() {
@@ -58,7 +55,11 @@ class App extends React.Component{
             <Paper style={{margin:16}}>
                 <List>
                     {this.state.items.map((item, idx)=> (
-                        <Todo item = {item} key ={item.id} delete = {this.delete}/>
+                        <Todo item = {item}
+                              key ={item.id}
+                              delete = {this.delete}
+                              update = {this.update}
+                        />
                     ))}
                 </List>
             </Paper>
--- src/Todo.js
+++ src/Todo.js
@@ -7,6 +7,7 @@ class Todo extends React.Component {
         super(props);
         this.state = { item: props.item , readOnly: true };
         this.delete = props.delete;
+        this.update = props.update;
     }
     deleteEventHandler = () => {
         this.delete(this.state.item);
@@ -20,6 +21,7 @@ class Todo extends React.Component {
     enterKeyEventHandler = (e) => {
         if(e.key==="Enter"){
             this.setState({readOnly:true});
+            this.update(this.state.item); //엔터를 누르면 저장
         }
     }
     editEventHandler = (e) => {
@@ -31,6 +33,7 @@ class Todo extends React.Component {
         const thisItem = this.state.item;
         thisItem.done = !thisItem.done;
         this.setState({item:thisItem});
+        this.update(this.state.item); //체크박스가 변경되면 저장
     }
     render() {
         const item = this.state.item;
